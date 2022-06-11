c4eb468e1790edc55e309f0b32de85b67ec51443
Fr0xty <ongjiae8@gmail.com>
2022-06-11 14:19:26 +0800

    bug fix: incorrectly extract access_token from cookie

--- src/server/routes/api/resource/resource.ts
+++ src/server/routes/api/resource/resource.ts
@@ -2,7 +2,7 @@
  * resource endpoint (protected route)
  */
 import { Router } from 'express';
-import JWT from 'jsonwebtoken';
+import JWT, { JwtPayload } from 'jsonwebtoken';
 import userRouter from './user.js';
 
 const router = Router();
@@ -10,11 +10,11 @@ const router = Router();
 /**
  * access token check
  */
-router.use(async (req, res) => {
+router.use(async (req, res, next) => {
     if (!req.signedCookies['at'] || !req.signedCookies['strategy']) return res.sendStatus(401);
-
     try {
-        req.accessToken = JWT.verify(req.signedCookies['at'], process.env.JWT_SECRET_KEY!).toString();
+        req.accessToken = (JWT.verify(req.signedCookies['at'], process.env.JWT_SECRET_KEY!) as JwtPayload).access_token;
+        return next();
     } catch {
         /**
          * tampered access token: reset session
